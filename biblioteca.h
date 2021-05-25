46ce6f25efb77e8b6ac65b8db35103a91ad75d20
Adley Rodrigues de Castro <51420674+AdleyRodrigues@users.noreply.github.com>
2021-05-24 21:41:11 -0300

    Add files via upload

---
+++ biblioteca.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.c>
+#include <time.h>
+
+#define t 4
+#define tamaF 4
+#define tamaM 5
+#define tamaD 6
+
+#define SUBIR 'w'
+#define DESCER 's'
+#define ESQUERDA 'a'
+#define DIREITA 'd'
+#define VAZIO ' '
+#define SALA '24'
